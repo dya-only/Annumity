3abcfaceeac2f33e1e0c7883b783732d1299eeac
dya-code <dyacode@proton.me>
2023-03-02 21:59:31 +0900

    mypage wish & watched anime list

--- src/frontend/src/pages/mypage.tsx
+++ src/frontend/src/pages/mypage.tsx
@@ -9,14 +9,125 @@ import Nav from './nav_default'
 function MyPage() {
 
   const [week, setWeek] = useState('')
-  const [infoSection, setInfoSection] = useState('wish')
-  const [isWishBtn, setIsWishBtn] = useState('mp_wish')
+  const [infoSection, setInfoSection] = useState('')
+  const [isWishBtn, setIsWishBtn] = useState('')
   const [isWatchedBtn, setIsWatchedBtn] = useState('')
   const [pageLoading, setPageLoading] = useState(true)
+  const [wishList, setWishList] = useState([])
+  const [watchedList, setWatchedList] = useState([])
+  
+  let searchValue2: string = ""
+  let PropTitle: string = ""
+  const [bgVideo, setBgVideo] = useState('')
+  const [openSearch, setOpenSearch] = useState(false)
+  const [windowLoad, setWindowLoad] = useState(true)
+  const [searchSel, setSearchSel] = useState({
+    name: '',
+    img: ''
+  })
+  const [searchSeries, setSearchSeries] = useState([])
+  const [searchRelated, setSearchRelated] = useState([])
+  const [selectedInfo, setSelectedInfo] = useState({
+    id: '',
+    name: '',
+    img: '',
+    content: '',
+    distribute: '',
+    year_quarter: '',
+    age: '',
+    genres: []
+  })
+
+  const getChangeOnSearch = (title: string) => {
+    // setSearchValue(title)
+    searchValue2 = title
+    setWindowLoad(true)
+    getSearch()
+    setOpenSearch(true)
+  }
+
+  const getInfo = async () => {
+    setTimeout(()=>{
+      setWindowLoad(false)
+    }, 1000)
+
+    const res = await fetch(`/api/info?name=${PropTitle}`, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    const data = await res.json()
+    console.log(data)
+
+    let genres_temp: any = []
+    for (let i = 0; i < 4; i++) {
+      genres_temp.push(data.anime.main_tag[i].name)
+    }
+
+    setSelectedInfo({
+      id: data.id,
+      name: data.anime.name,
+      img: data.anime.img,
+      content: data.anime.content,
+      distribute: data.anime.animation_info.distributed_air_time,
+      year_quarter: data.anime.animation_info.air_year_quarter,
+      age: data.anime.content_rating,
+      genres: genres_temp
+    })
+  }
+
+  const getSearch = async () => {
+    setTimeout(()=>{
+      setWindowLoad(false)
+    }, 2000)
+
+    const res = await fetch(`/api/search?name=${searchValue2}`, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+
+    const data = await res.json()
+
+    if (data.anime !== 'not found') {
+      setSearchSel({
+        name: data.anime.name,
+        img: data.anime.img
+      })
+
+      let genres_temp: any = []
+      for (let i = 0; i < data.anime.main_tag.length; i++) {
+        genres_temp.push(data.anime.main_tag[i].name)
+      }
+      setSelectedInfo({
+        id: data.anime.id,
+        name: data.anime.name,
+        img: data.anime.img,
+        content: data.anime.content,
+        distribute: data.anime.animation_info.distributed_air_time,
+        year_quarter: data.anime.animation_info.air_year_quarter,
+        age: data.anime.content_rating,
+        genres: genres_temp
+      })
+      setBgVideo(await data.pv)
+      console.log(await data.pv)
+
+      setSearchSeries(await data.anime.series_item)
+      setSearchRelated(await data.anime.related_item)
+      console.log("searching props: ", searchSeries)
+    } else {
+      setSearchSel({
+        name: '검색 결과가 존재하지 않습니다.',
+        img: ''
+      })
+    }
+  }
 
   const WishBtnEvent = () => {
     setInfoSection('wish')
-    setIsWishBtn('mp_wish')
+    setIsWishBtn('myp_wish')
     setIsWatchedBtn('')
   }
   const WatchedBtnEvent = () => {
@@ -34,7 +145,8 @@ function MyPage() {
     })
     const data = await res.json()
 
-    console.log(data)
+    setWishList(data.wishDATA)
+    setWatchedList(data.watchedDATA)
   }
 
   useEffect(() => {
@@ -43,13 +155,98 @@ function MyPage() {
     setWeek(WeekDay[day.getDay()])
 
     getUserDB()
-
-    setIsWishBtn('mp_wish')
     setTimeout(() => setPageLoading(false), 1000);
   }, [])
 
   return (
     <Fragment>
+
+      { openSearch ?
+        <div className="window-contain">
+          <div>
+            <div className="search-title-window">
+              <div className="window-title">검색 결과</div>
+              <div className="window-btns">
+                <button className='info-in-btn' onClick={() => { setOpenSearch(false); setWindowLoad(true) }}><FontAwesomeIcon className='window-btn' icon={faXmark} /></button>
+              </div>
+            </div>
+            <div className="search-info-window">
+              { windowLoad ?
+                <div className="loading-window">
+                  <img className='spinner' src={`images/logo.png`} alt="" />
+                </div>
+              :
+                <div className='search-contain'>
+                  <div className="search-el">
+                    <div className="search-bg"></div>
+                    <ReactPlayer
+                      className='react-player'
+                      url={bgVideo}
+                      width={'94%'}
+                      height={'100%'}
+                      playing={true}
+                      muted={true}
+                      controls={false}
+                      light={false}
+                      pip={false}
+                      loop={true}
+                    />
+                    <div className="search-ui">
+                      <img className='search-main-img' src={ selectedInfo.img } alt="" />
+                      <div className="">
+                        <div className="search-title">{ selectedInfo.name }</div>
+                        <div className="search-tags-info">
+                          { selectedInfo.genres.map((el: any, idx: number) => (
+                            <div key={idx} className="search-tag">#{el}</div>
+                          )) }
+                        </div>
+                        <div className="lighter">방영 요일: <strong>{ selectedInfo.distribute || '정보없음' }</strong></div>
+                        <div className="lighter">출시 구분: <strong>{ selectedInfo.year_quarter }</strong></div>
+                        <div className="lighter">장르: <strong>{ selectedInfo.genres[0] } · { selectedInfo.genres[1] }</strong></div>
+                        <div className="card-btn-contain">
+                          <button className='play-search' onClick={ () => window.open(`https://laftel.net/item/${selectedInfo.id}`) }>
+                            {/* <div className="laftel-logo"></div> */}
+                            <img className='laftel-logo' src={`images/laftel.png`} alt="" />
+                            <div className="play-text">보러가기</div>
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="search-down">
+                    <div className="series-title">같은 시리즈의 작품</div>
+                    <div className="series-contain">
+                      { searchSeries.map((el: any, idx: number) => (
+                          <button key={idx} className="search-card" onClick={() => getChangeOnSearch(el.name)}>
+                            <img className='search-card-img' src={ el.images[0].img_url || '' } alt='' />
+                            <div className="text-contain">
+                              <div className="search-card-title">{ el.name }</div>
+                            </div>
+                          </button>
+                        )) }
+                    </div>
+
+                    <div className="series-title">비슷한 작품</div>
+                    <div className="series-contain">
+                      { searchRelated.map((el: any, idx: number) => (
+                          <button key={idx} className="search-card" onClick={() => getChangeOnSearch(el.name)}>
+                            <img className='search-card-img' src={ el.images[0].img_url || '' } alt='' />
+                            <div className="text-contain">
+                              <div className="search-card-title">{ el.name }</div>
+                            </div>
+                          </button>
+                        )) }
+                    </div>
+                  </div>
+                </div>
+              }
+            </div>
+          </div>
+
+        </div>
+      : null }
+
       <Nav />
 
       <main className='mypage_main'>
@@ -72,13 +269,30 @@ function MyPage() {
           <button className={`section-btn ${isWatchedBtn}`} onClick={WatchedBtnEvent}>정주행완료</button>
         </div>
         { infoSection == 'wish' ?
-          <div className="wish-contents">
-            wish Contents
+          <div className="section-contents">
+            { wishList.map((el: any, idx: number) => (
+              <button key={idx} className="search-card" onClick={() => getChangeOnSearch(el.anime.name)}>
+                <img className='search-card-img' src={ el.anime.img || '' } alt='' />
+                <div className="text-contain">
+                  <div className="search-card-title">{ el.anime.name }</div>
+                </div>
+              </button>
+            )) }
           </div>
         :
-          <div className="watched-contents">
-            watched Contents
-          </div>
+          ( infoSection == 'watched' ?
+            <div className="section-contents">
+              { watchedList.map((el: any, idx: number) => (
+              <button key={idx} className="search-card" onClick={() => getChangeOnSearch(el.anime.name)}>
+                <img className='search-card-img' src={ el.anime.img || '' } alt='' />
+                <div className="text-contain">
+                  <div className="search-card-title">{ el.anime.name }</div>
+                </div>
+              </button>
+            )) }
+            </div>
+          : <div className="section-contents">버튼을 클릭해주세요.</div>
+          )
         }
       </main>
     </Fragment>
--- src/routes/db/view.ts
+++ src/routes/db/view.ts
@@ -1,26 +1,45 @@
 import express from 'express'
 import sqlite3 from 'sqlite3'
+import laftel from 'laftel.js'
 
 const router = express.Router()
 
 router.route("/").get(async (req, res) => {
   const params = req.query as any
   const db = new sqlite3.Database('db/user.db')
-  let wishID: number[] = []
-  let watchedID: number[] = []
-
+  
   let sqlREAD = `SELECT * FROM user WHERE email=\"${params.email || ''}\"`
-
-  db.all(sqlREAD, [], (e, rows) => {
+  
+  db.all(sqlREAD, [], async (e, rows) => {
     if (e) throw e
 
+    let wishID: number[] = []
+    let watchedID: number[] = []
+    let wishDATA: any[] = []
+    let watchedDATA: any[] = []
+  
     wishID = JSON.parse(rows[0].wish_id)
     watchedID = JSON.parse(rows[0].watched_id)
-    
-    res.status(200).json({ wish: wishID, watched: watchedID })
-  })
+  
+    // wish loop
+    for (let i = 0; i < wishID.length; i++) {
+      await laftel.getItem(wishID[i].toString()).then(result => {
+        wishDATA.push({ anime: result })
+      })
+    }
 
+    // watched loop
+    for (let i = 0; i < watchedID.length; i++) {
+      await laftel.getItem(watchedID[i].toString()).then(result => {
+        watchedDATA.push({ anime: result })
+      })
+    }
+      
+    res.status(200).json({ wishID: wishID, watchedID: watchedID, wishDATA: wishDATA, watchedDATA: watchedDATA })
+  })
+  
   db.close()
+
 })
 
 export default router
