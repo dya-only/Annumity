752616bf7ad045c3ae73183ffbf880bd97346f19
dya-only <dyacode@proton.me>
2023-03-24 20:15:38 +0900

    daily aws dns

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://ec2-15-165-235-223.ap-northeast-2.compute.amazonaws.com:3000',
+        target: 'http://ec2-43-201-0-230.ap-northeast-2.compute.amazonaws.com:3000',
         changeOrigin: true,
         secure: false,
         ws: true,
